e7570bdd4f48f0e00ab50cfa126c526410928aab
LOGAN KONOPATZKE <logankono9080@gmail.com>
2022-12-04 00:33:16 -0600

    remove useless variable

--- day_4b/src/main.rs
+++ day_4b/src/main.rs
@@ -1,10 +1,7 @@
 fn main() {
     let input = include_str!("../input.txt");
 
-    let mut idx = 0;
-
     let sum = input.lines().fold(0, |acc, x| {
-        idx += 1;
         let comma = x.find(",").unwrap();
         let first = &x[0..comma];
         let second = &x[comma + 1..x.len()];
