24ecdb670bd96661891b60c8697a06433d8b68ec
LOGAN KONOPATZKE <logankono9080@gmail.com>
2022-12-04 00:44:56 -0600

    code cleanup

--- day_4a/src/main.rs
+++ day_4a/src/main.rs
@@ -1,25 +1,14 @@
 fn main() {
     let input = include_str!("../input.txt");
 
-    let mut idx = 0;
-
     let sum = input.lines().fold(0, |acc, x| {
-        idx += 1;
-        let comma = x.find(",").unwrap();
-        let first = &x[0..comma];
-        let second = &x[comma + 1..x.len()];
+        let (first, second) = x.split_once(",").unwrap();
 
-        let dash = first.find("-").unwrap();
-        let first = (
-            &first[0..dash].parse::<i32>().unwrap(),
-            &first[dash + 1..first.len()].parse::<i32>().unwrap(),
-        );
+        let first = first.split_once("-").unwrap();
+        let second = second.split_once("-").unwrap();
 
-        let dash = second.find("-").unwrap();
-        let second = (
-            &second[0..dash].parse::<i32>().unwrap(),
-            &second[dash + 1..second.len()].parse::<i32>().unwrap(),
-        );
+        let first: (i32, i32) = (first.0.parse().unwrap(), first.1.parse().unwrap());
+        let second: (i32, i32) = (second.0.parse().unwrap(), second.1.parse().unwrap());
 
         let mut add = 0;
         if (first.0 >= second.0 && first.1 <= second.1)
